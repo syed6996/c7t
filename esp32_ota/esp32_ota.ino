dea4b4f4eeb0d71fff50766083cb9b5ccebbee0f
syed6996 <abuthalibsn2@gmail.com>
2023-02-01 23:27:45 +0530

    Initial commit
    testing for the initial commit

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -11,10 +11,10 @@ const char * ssid = "ACTFIBERNET";
 const char * password = "act12345";
 
 String FirmwareVer = {
-  "2.1"
+  "2.2"
 };
 #define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/esp32_ota.ino.esp32.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
