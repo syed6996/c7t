943f4681f5cd0c4c59702f8919176eac4fc22fd6
syed6996 <abuthalibsn2@gmail.com>
2023-02-01 23:35:18 +0530

    intial firas

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -13,8 +13,8 @@ const char * password = "act12345";
 String FirmwareVer = {
   "2.2"
 };
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/esp32_ota.ino.esp32.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/syed6996/c7t/main/esp32_ota/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/syed6996/c7t/main/esp32_ota/bin_version.txt"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
